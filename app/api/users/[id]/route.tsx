3ddca73460c369c3bdf5a950e52a3bdcf591f2c1
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-04-28 11:13:55 -0700

    delete on object

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -20,3 +20,14 @@ export async function PUT(
     return NextResponse.json({ error: "user not found" }, { status: 404 });
   return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
 }
+
+export function DELETE(
+  request: NextRequest,
+  { params }: { params: { id: number } }
+) {
+  // Checking if the user exist in db.
+  if (params.id > 10)
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  // Returning an empty obj
+  return NextResponse.json({});
+}
