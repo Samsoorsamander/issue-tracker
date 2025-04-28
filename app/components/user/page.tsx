443d3b21d69fa3598ca8c007cd0002516decd8f1
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-04-27 22:16:11 -0700

    routing

--- app/components/user/page.tsx
+++ app/components/user/page.tsx
@@ -1,10 +1,5 @@
 import React from "react";
-
-interface Users {
-  id: number;
-  name: string;
-  email: string;
-}
+import UserTable from "./UserTable";
 
 const UsersPage = async () => {
   // Insted of fetching data in client side we can easly fetch data in server side
@@ -12,30 +7,11 @@ const UsersPage = async () => {
 
   //In next the fetch has its own default catching of data which we can disabel as will
   // revalidate means catch data in every 10 sec
-  const res = await fetch("http://jsonplaceholder.typicode.com/users", {
-    next: { revalidate: 10 },
-  });
-  const users: Users[] = await res.json();
 
   return (
     <>
       <h1>Users</h1>
-      <table className="tabel table-zebra">
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Email</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user.id}>
-              <td>{user.name}</td>
-              <td>{user.email}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+      <UserTable />
     </>
   );
 };
