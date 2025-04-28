76c73fc6d22cb2da674994b0aa2cbc7b1181681b
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-04-28 07:13:29 -0700

    creating a user

--- app/api/users/route.tsx
+++ app/api/users/route.tsx
@@ -12,3 +12,10 @@ export function GET(request: NextRequest) {
     },
   ]);
 }
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  if (!body.name)
+    NextResponse.json({ error: "name is required" }, { status: 400 });
+  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+}
