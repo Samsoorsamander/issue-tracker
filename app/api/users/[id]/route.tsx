8736406dcefc1d46bb0df8f163bbd4f58975c63f
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-04-29 20:52:19 -0700

    update a user

--- app/api/users/[id]/route.tsx
+++ app/api/users/[id]/route.tsx
@@ -16,7 +16,7 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   const body = await request.json();
   const validation = schema.safeParse(body);
@@ -25,10 +25,20 @@ export async function PUT(
 
   if (!validation.success)
     return NextResponse.json(validation.error.errors, { status: 400 });
-  if (params.id > 10) {
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+  if (!user) {
     return NextResponse.json({ error: "user not found" }, { status: 404 });
   }
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(updatedUser, { status: 201 });
 }
 
 export function DELETE(
