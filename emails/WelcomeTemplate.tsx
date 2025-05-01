c45493ca8405d9995443ed269acc6a76c30881e1
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-05-01 06:07:38 -0700

    styling email template

--- emails/WelcomeTemplate.tsx
+++ emails/WelcomeTemplate.tsx
@@ -1,15 +1,26 @@
 import React from "react";
-import { Html, Body, Container, Text, Preview } from "@react-email/components";
+import {
+  Html,
+  Body,
+  Container,
+  Tailwind,
+  Text,
+  Preview,
+  Link,
+} from "@react-email/components";
 
 const WelcomeTemplate = ({ name }: { name: string }) => {
   return (
     <Html>
       <Preview>Welcome on board!</Preview>
-      <Body>
-        <Container>
-          <Text>Hello {name}</Text>
-        </Container>
-      </Body>
+      <Tailwind>
+        <Body>
+          <Container>
+            <Text className="font-bold text-3xl">Hello {name}</Text>
+            <Link href="samsoor.com">www.samsoor.com</Link>
+          </Container>
+        </Body>
+      </Tailwind>
     </Html>
   );
 };
