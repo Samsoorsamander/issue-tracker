4dc9d0b91710dbcbf219af19f5c55fdb2363d558
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-05-04 06:49:34 -0700

    style links

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -2,16 +2,23 @@ import Link from "next/link";
 import React from "react";
 
 const NavBar = () => {
+  const link = [
+    { label: "Dashboard", href: "/" },
+    { label: "Issue", href: "/issue" },
+  ];
   return (
-    <nav className="flex text-black">
+    <nav className="flex text-black mb-5 space-x-5 border-b h-14 px-5 items-center">
       <Link href={"/"}>Logo</Link>
-      <ul>
-        <li>
-          <Link href={"/"}>Dashboard</Link>
-        </li>
-        <li>
-          <Link href={"/issue"}>Issue</Link>
-        </li>
+      <ul className="flex space-x-5">
+        {link.map((link) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="text-zinc-500 hover:text-zinc-800 transition-colors"
+          >
+            {link.label}
+          </Link>
+        ))}
       </ul>
     </nav>
   );
