ff9cb9e07f3be267d1331df88c9514632033e18e
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-05-05 09:30:56 -0700

    set up react hook form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,15 +1,28 @@
 "use client";
 import { Button, TextField } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
+import { Controller, Form, useForm } from "react-hook-form";
 import "easymde/dist/easymde.min.css";
-const newIssue = () => {
+
+interface IssueForm {
+  title: string;
+  description: string;
+}
+const NewIssuePage = () => {
+  const { register, control } = useForm<IssueForm>();
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root size="3" placeholder="Title" />
-      <SimpleMDE placeholder="Description" />
+    <Form className="max-w-xl space-y-3">
+      <TextField.Root size="3" placeholder="Title" {...register("title")} />
+      <Controller
+        control={control}
+        name="description"
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
       <Button>Submit New Issue</Button>
-    </div>
+    </Form>
   );
 };
 
-export default newIssue;
+export default NewIssuePage;
