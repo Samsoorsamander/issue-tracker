33fa83c7014149fd6f5f9a1281e102aed85ac588
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-05-07 19:56:42 -0700

    add skeleton

---
+++ app/issues/[id]/loading.tsx
@@ -0,0 +1,22 @@
+import { Flex, Card, Box } from "@radix-ui/themes";
+import Skeleton from "react-loading-skeleton";
+import "react-loading-skeleton/dist/skeleton.css";
+
+const loading = () => {
+  return (
+    <Box className="max-w-xl">
+      <Skeleton />
+      <Flex gap="3" my="2">
+        <Skeleton width="5rem" />
+        <Skeleton width="8rem" />
+      </Flex>
+      <Card className="prose" mt="4">
+        <Skeleton />
+        <Skeleton />
+        <Skeleton />
+      </Card>
+    </Box>
+  );
+};
+
+export default loading;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,6 +1,7 @@
 import IssueStatusBadge from "@/app/components/IssueStatusBadge";
 import { prisma } from "@/prisma/client";
 import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import delay from "delay";
 import { notFound } from "next/navigation";
 import MarkDown from "react-markdown";
 
@@ -14,7 +15,7 @@ const IssueDetailesPage = async ({ params }: Props) => {
   });
 
   if (!issue) notFound();
-
+  await delay(2000);
   return (
     <div>
       <Heading>{issue.title}</Heading>
