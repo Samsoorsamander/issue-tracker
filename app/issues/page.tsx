3e7c3b91282b979bf6efb479ca92f8195d1b550b
S@msoor Samander <154373431+Samsoorsamander@users.noreply.github.com>
2025-05-11 11:40:54 -0700

    Filtering Issues is not working as expected. will see it later

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,9 +2,22 @@ import { IssueStatusBadge, Link } from "@/app/components";
 import { prisma } from "@/prisma/client";
 import { Table } from "@radix-ui/themes";
 import IssueAction from "./IssueAction";
+import { Status } from "../generated/prisma";
 
-const IssuePage = async () => {
-  const issue = await prisma.issue.findMany();
+interface Props {
+  searchParams: { status: Status };
+}
+
+const IssuePage = async ({ searchParams }: Props) => {
+  const statuses = Object.values(Status);
+  const status = statuses.includes(searchParams.status)
+    ? searchParams.status
+    : undefined;
+  const issue = await prisma.issue.findMany({
+    where: {
+      status,
+    },
+  });
 
   return (
     <div>
